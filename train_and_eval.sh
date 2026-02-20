ee9865f3319db6c95b3298bf89d944d136b88714
Skyler Deutsch <153780099+skylerdeutsch@users.noreply.github.com>
2026-02-19 18:28:03 -0800

    Update train_and_eval.sh
    Changed pathways and removed GPU line - using CPU.

--- train_and_eval.sh
+++ train_and_eval.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-DATASET_ROOT="/mnt/ResearchPhysics/zz_Qifan_Xu/datasets/PathMNIST"
-OUTPUT_ROOT="/mnt/ResearchPhysics/zz_Qifan_Xu/datasets/PathMNIST_ResNet18"
+DATASET_ROOT="/Users/sdeutsch/Library/CloudStorage/OneDrive-UCSF/Desktop/BIOE\ 245/Homework/Homework\ 2"
+OUTPUT_ROOT="/Users/sdeutsch/Library/CloudStorage/OneDrive-UCSF/Desktop/BIOE\ 245/Homework/Homework\ 2"
 
 python ./train_and_eval.py \
     --download \
     --output_root ${OUTPUT_ROOT} \
-    --gpu_ids 2 \
     --dataset_root ${DATASET_ROOT}
