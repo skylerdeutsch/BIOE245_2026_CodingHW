29f20f4d6ad3b58d90b51f07953c43b8992a725f
Skyler Deutsch <153780099+skylerdeutsch@users.noreply.github.com>
2026-02-19 18:29:37 -0800

    Update train_and_eval.sh
    Fixed pathway syntax

--- train_and_eval.sh
+++ train_and_eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DATASET_ROOT="/Users/sdeutsch/Library/CloudStorage/OneDrive-UCSF/Desktop/BIOE\ 245/Homework/Homework\ 2"
-OUTPUT_ROOT="/Users/sdeutsch/Library/CloudStorage/OneDrive-UCSF/Desktop/BIOE\ 245/Homework/Homework\ 2"
+DATASET_ROOT="/Users/sdeutsch/Library/CloudStorage/OneDrive-UCSF/Desktop/BIOE 245/Homework/Homework 2"
+OUTPUT_ROOT="/Users/sdeutsch/Library/CloudStorage/OneDrive-UCSF/Desktop/BIOE 245/Homework/Homework 2"
 
 python ./train_and_eval.py \
     --download \
